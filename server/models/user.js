b9119b32e16c98068e44d6fcb128b6a33d992d02
s-r-aman <s.r.aman.sra@gmail.com>
2017-12-13 01:28:11 +0530

    Added post user login route.

--- server/models/user.js
+++ server/models/user.js
@@ -87,6 +87,25 @@ UserSchema.statics.findByToken = function (token){
 
 };
 
+UserSchema.statics.findByCredentials = function (email, password){
+  let User = this;
+
+  return User
+  .findOne({email})
+  .then(user => {
+    if(!user) return Promise.reject();
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, result) => {
+        if(result){
+          resolve(user);
+        }else{
+          reject();
+        }
+      });
+    });
+  });
+};
+
 UserSchema.pre('save', function(next){
   let user = this;
   let password = user.password;
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ require('./config/config.js');
 const express = require('express');
 const bodyParser = require("body-parser");
 const _ = require('lodash');
-
+const bcrypt = require('bcryptjs');
 const { mongoose } = require("./db/mongoose");
 const { Todo } = require("./models/todo");
 const { User } = require('./models/user');
@@ -108,6 +108,22 @@ app.post('/users', (req, res) => {
 
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);    
+});
+
+//login route
+
+app.post('/users/login', (req, res) => {
+    
+    body = _.pick(req.body, ['email', 'password']);
+
+    User
+    .findByCredentials(body.email, body.password)
+    .then((user) => {
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        });
+    }).catch((err) => res.status(400).send());
+
 });
 
 app.listen(3000, () => console.log(`Started on port ${port}`));
